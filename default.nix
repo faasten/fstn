86b394121f7b5ed787c1a1ca56afb28a237402da
Amit Aryeh Levy <amit@amitlevy.com>
2022-08-29 16:44:21 -0400

    nit

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {}, release ? true }:
 
 with pkgs;
-let cargo_nix = callPackage ./Cargo.nix {};
+let cargo_nix = import ./Cargo.nix { inherit pkgs; };
 in cargo_nix.rootCrate.build
